c99f87685b4fbda76d0df7983c44bd4b953f9a45
Melinda Po <mellypo@gmail.com>
2016-01-14 17:29:37 -0800

    Initial Commit for Heroku

--- punchstarter/default_settings.py
+++ punchstarter/default_settings.py
@@ -1,15 +1,17 @@
-import os
+import os 
 import cloudinary
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/newapp.db"
-
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
+SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI',"sqlite:///"+ BASE_DIR + "/app.db")
+SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS',True)
+CLOUDINARY_CLOUD_NAME=os.environ.get('CLOUDINARY_CLOUD_NAME',"da2bogsxm")
+CLOUDINARY_API_KEY=os.environ.get('CLOUDINARY_API_KEY',"566777878874789")
+CLOUDINARY_API_SECRET=os.environ.get('CLOUDINARY_API_SECRET',"ypsxy24L7Ld4wi7oIPYk")
 
 cloudinary.config( 
-  cloud_name = "da2bogsxm", 
-  api_key = "566777878874789", 
-  api_secret = "ha4fe7-ypsxy24L7Ld4wi7oIPYk" 
+  cloud_name = CLOUDINARY_CLOUD_NAME, 
+  api_key = CLOUDINARY_API_KEY, 
+  api_secret = CLOUDINARY_API_SECRET 
 )
+
